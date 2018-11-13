8c5f3fe7f60cbbef4e5634afe5d12496659d4d6a
aikawa321 <guilty.gate0619@gmail.com>
2018-11-13 00:05:07 +0000

    Add some controllers and models

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem "refile", require: "refile/rails", github: 'manfe/refile'
+gem "refile-mini_magick"
+gem 'devise'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 # Use sqlite3 as the database for Active Record
@@ -60,3 +64,10 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem'kaminari'
+
+gem 'bootstrap-sass'
+gem 'jquery-rails'
+gem 'ransack'
+gem 'activeadmin'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  resources :cms, :only => [:index,:trend,:search]
+  resources :cd_products, :only =>[:index,:show,:era_search,:era_search_list,:search,:list]
+  devise_for :admins
+  resources :goods, :only =>[:new,:create,:index,:edite,:update,:destroy]
+  resources :companeys, :only =>[:new,:create,:index,:edit,:update,:destroy]
+  resources :doramas, :only => [:new,:create,:index,:edit,:update,:destroy,:feature,:title_search]
+  resources :movies, :only =>[:new,:create,:index,:edit,:update,:destroy,:feature,:title_search]
+  namespace :admins do
+    resource :artist, only: [:new,:create,:index,:edit,:update,:destroy]
+  end
+  resources :admins
+  resources :bag_items, :only =>[:index,:edit,:update,:destroy]
+  resources :shopping_bags, :only =>[:new,:create,:index,:show,:destroy]
+  resources :cart_item, :only =>[:add,:destroy,:update]
+  resources :shopping_carts, :only => [:new,:create,:index,:show,:destroy]
+  resources :address_lines, :only =>[:new,:create,:index,:edit,:update,:destroy]
+  resources :favorites, :only =>[:create,:destroy,:index]
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
