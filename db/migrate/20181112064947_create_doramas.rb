4f3c6a54789e8167c749ffb9ec585fff979e8861
SumikkoTokage <tm.nr.smanml@gmail.com>
2018-11-19 10:59:29 +0000

    fix_migrationfile

--- db/migrate/20181112064947_create_doramas.rb
+++ db/migrate/20181112064947_create_doramas.rb
@@ -3,6 +3,7 @@ class CreateDoramas < ActiveRecord::Migration[5.2]
     create_table :doramas do |t|
       t.string :name
       t.integer :title_head_id
+      t.text :dorama_image_url
       t.date :dorama_start_date
       t.integer :week_id
 
