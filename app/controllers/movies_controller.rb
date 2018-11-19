8a9edeac5d72a520b3fab799d2d5a7df24b5e1a4
pesopeso1234 <ito.taishi.0322@gmail.com>
2018-11-19 10:59:04 +0000

    tentative-commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,9 @@
 class MoviesController < ApplicationController
 
   def index
+  	@cd_products = CdProduct.where("movie_id not ?", nil).where(sale_status_id: 2).order(release_date: :desc).page(params[:page]).per(9)
+  	@movies = Movie.all
+
   end
 
   def title_search
