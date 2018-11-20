6dc0dff6b31147c2f40293c9da7f6d79041bbad3
SumikkoTokage <tm.nr.smanml@gmail.com>
2018-11-20 09:15:23 +0000

    画像関連

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
 	enum title_head_id:{
 		あ:1,か:2,さ:3,た:4,な:5,は:6,ま:7,や:8,ら:9,わ:10
 	}
+	#adminページの画像挿入処理
+	has_attached_file :movie_image, styles: { medium: "300x300>", thumb: "100x100>" }
+	validates_attachment :movie_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
 end
