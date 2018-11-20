6296fb84c20425f249301c7f293e9ae9b9ea45c4
pesopeso1234 <ito.taishi.0322@gmail.com>
2018-11-20 04:49:05 +0000

    modified view & controller

--- app/controllers/doramas_controller.rb
+++ app/controllers/doramas_controller.rb
@@ -1,6 +1,8 @@
 class DoramasController < ApplicationController
 
   def index
+  	@cd_products = CdProduct.where("dorama_id not ?", nil).where(sale_status_id: 2).order(release_date: :desc).page(params[:page]).per(9)
+  	@doramas = Dorama.all
   end
 
   def feature
