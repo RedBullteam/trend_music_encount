766b75183d1c191103bd9bcd8c32eda022b82862
yuki-koike <k0001yuuki@gamil.com>
2018-11-24 05:19:05 +0000

    add_era_search

--- app/controllers/cd_products_controller.rb
+++ app/controllers/cd_products_controller.rb
@@ -10,10 +10,10 @@ include Search
     @discs = Disc.where(cd_product_id: (params[:id]))
     @songs = Song.where(disc_id: @discs.ids)
     @cd_products = CdProduct.where(artist_id: (params[:id])).page(params[:page]).per(5)
-
   end
 
   def era_search
+
   end
 
   def era_search_list
