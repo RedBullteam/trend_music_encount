57dcc9d7297d83ea31ebc04ce6e57c834919f822
yuki-koike <k0001yuuki@gamil.com>
2018-11-25 13:44:59 +0000

    dorama

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,10 @@ Rails.application.routes.draw do
   resources :cms, :only => [:index,:trend,:search]
   get '/era_search' => 'cd_products#era_search'
   get '/release_era_tag/:id' => 'release_era_tags#show', as: 'release_era_tag'
-  get '/era_search/list' => 'cd_products#era_search_list'
   get '/cd_products/:id' => 'cd_products#show', as: 'cd_product'
   get '/search/list' => 'cd_products#list', as: :search_list
   get '/doramas/title_search/:id/:dorama_name' => "doramas#title_search", as: :dorama_title_search
+  get '/doramas/feature' => 'doramas#feature', as: 'doramas_feature'
   get '/movies/title_search/:id/:movie_name' => "movies#title_search", as: :movie_title_search
   get '/cms/goods_search' => "cms#goods_search", as: :cm_goods_search
   get '/cms/comany_search' => "cms#company_search", as: :cm_company_search
