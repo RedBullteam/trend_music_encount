a3c316c1e714e2b0b953a71c77729ccef7d90997
aikawa321 <guilty.gate0619@gmail.com>
2018-11-25 18:25:09 +0000

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'cd_products#index'
   get '/guide' => 'users#guide'
  namespace :admin do
-    resources :goods, :only =>[:new,:create,:index,:edite,:update,:destroy]
+    resources :commodities, :only =>[:new,:create,:index,:edite,:update,:destroy]
     resources :companeys, :only =>[:new,:create,:index,:edit,:update,:destroy]
     resources :doramas, :only =>[:new,:create,:index,:edit,:update,:destroy]
     resources :movies, :only =>[:new,:create,:index,:edit,:update,:destroy]
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
   get '/search/list' => 'cd_products#list', as: :search_list
   get '/doramas/title_search/:id/:dorama_name' => "doramas#title_search", as: :dorama_title_search
   get '/movies/title_search/:id/:movie_name' => "movies#title_search", as: :movie_title_search
-  get '/cms/goods_search' => "cms#goods_search", as: :cm_goods_search
+  get '/cms/commodity_search' => "cms#commodity_search", as: :cm_commodity_search
   get '/cms/company_search' => "cms#company_search", as: :cm_company_search
   get '/cd_products/search/artists_search_form' => "cd_products#artists_search_form", as: :artists_search_form
   get '/cd_products/search/songs_search_form' => "cd_products#songs_search_form", as: :songs_search_form
@@ -48,7 +48,7 @@ Rails.application.routes.draw do
  post 'users/sign_up/complete' => 'users/registrations#complete'
 end
 
-  resources :goods, :only =>[:new,:create,:index,:edite,:update,:destroy]
+  resources :commodities, :only =>[:new,:create,:index,:edite,:update,:destroy]
   resources :companeys, :only =>[:new,:create,:index,:edit,:update,:destroy]
   resources :doramas, :only => [:new,:create,:index,:edit,:update,:destroy,:feature,:title_search]
   resources :movies, :only =>[:new,:create,:index,:edit,:update,:destroy,:feature,:title_search]
