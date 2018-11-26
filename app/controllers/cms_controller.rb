8d87274ca8cd7adb8dba41101dc948d1d48d7123
pesopeso1234 <ito.taishi.0322@gmail.com>
2018-11-26 05:09:52 +0000

    complete feature-search

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -7,7 +7,7 @@ include Search
   end
 
   def trend
-      @videos = Video.all
+     @videos = Video.all
    @cd_products = CdProduct.where("commodity_id not ?", nil).where(sale_status_id: 2).order(release_date: :desc).page(params[:page]).per(9)
   end
 
@@ -16,8 +16,9 @@ include Search
     if search_name == ""
       return redirect_to cms_path
     end
-  	@commodities = commodities_name_search_function(search_name)
-    @commodities_cd_product = CdProduct.where(commodity_id:@commodity.ids).page(params[:page]).per(12)
+  	@commodities = commodity_name_search_function(search_name)
+    @match_records = CdProduct.where(commodity_id:@commodities.ids).where(sale_status_id: 2).count
+    @commodities_cd_products = CdProduct.where(commodity_id:@commodities.ids).where(sale_status_id: 2).page(params[:page]).per(12)
     @path = controller_path + '#' + action_name
     def @path.is(*str)
         str.map{|s| self.include?(s)}.include?(true)
@@ -31,9 +32,9 @@ include Search
       return redirect_to cms_path
     end
     @companies = company_name_search_function(search_name)
-    @companies_goods = Good.where(company_id:@companies.ids)
-    @match_records = CdProduct.where(good_id:@companies_goods.ids).where(sale_status_id: 2).count
-    @companies_cd_products = CdProduct.where(good_id:@companies_goods.ids).where(sale_status_id: 2).page(params[:page]).per(12)
+    @companies_commodities = Commodity.where(company_id:@companies.ids)
+    @match_records = CdProduct.where(commodity_id:@companies_commodities.ids).where(sale_status_id: 2).count
+    @companies_cd_products = CdProduct.where(commodity_id:@companies_commodities.ids).where(sale_status_id: 2).page(params[:page]).per(12)
     @path = controller_path + '#' + action_name
     def @path.is(*str)
         str.map{|s| self.include?(s)}.include?(true)
--- app/controllers/concerns/search.rb
+++ app/controllers/concerns/search.rb
@@ -17,28 +17,17 @@ module Search
 		CdProduct.where(dorama_id: id).where(sale_status_id: 2).order(release_date: :desc).page(params[:page]).per(12)
 	end
 
-	def commodity_match_record_function(search_name)
-		@search_cd_products = "no-matched"
-		@search_cd_products = Commodity.includes(:cd_products).where("commodity LIKE ?", "%" + search_name + "%").where(:cd_products => {:sale_status_id => 2}).count
-		return @search_cd_products
-	end
-
 	def commodity_name_search_function(search_name)
-		@search_cd_products = "no-matched"
-		@search_cd_products = Commodity.includes(:cd_products).where("commodity LIKE ?", "%" + search_name + "%").where(:cd_products => {:sale_status_id => 2}).order("cd_products.release_date desc")
-		return @search_cd_products
+		@search_commodities = "no-matched"
+		@search_commodities = Commodity.includes(:cd_products).where("commodity LIKE ?", "%" + search_name + "%").order("cd_products.release_date desc")
+		return 	@search_commodities
 	end
 
-	def company_match_record_function(search_name)
-		@search_cd_products = "no-matched"
-		@search_cd_products = CdProduct.joins({:commodity => :companies}).where("company LIKE ?", "%" + search_name + "%").where(sale_status_id: 2).count
-		return @search_cd_products
-	end
 
 	def company_name_search_function(search_name)
-		@search_cd_products = "no-matched"
-		@search_cd_products = CdProduct.joins({:commodities => :companies}).where("company LIKE ?", "%" + search_name + "%").where(sale_status_id: 2).order(:cd_products => {:release_date => :desc}).page(params[:page]).per(12)
-		return @search_cd_products
+		@search_companies = "no-matched"
+		@search_companies = Company.includes({:commodities => :cd_products}).where("company LIKE ?", "%" + search_name + "%").order("cd_products.release_date desc")
+		return 	@search_companies
 	end
 
 
