2fd03398e0fc9ee51a38adddb323f8a63766d91b
aikawa321 <guilty.gate0619@gmail.com>
2018-11-27 10:59:40 +0000

    cm controller update

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -7,35 +7,38 @@ include Search
   end
 
   def trend
-   @cd_products = CdProduct.where("commodity_id not ?", nil).where(sale_status_id: 2).order(release_date: :desc).page(params[:page]).per(9)
+   @cd_products = CdProduct.where("commodity_id not ?", nil).where(sale_status_id: 2).order(release_date: :desc).limit(9)
   end
 
   def commodity_search
-  	search_name = params[:search_name]
-    if search_name == ""
-      return redirect_to cms_path
-    end
-  	@commodities = commodities_name_search_function(search_name)
-    @commodities_cd_product = CdProduct.where(commodity_id:@commodity.ids).page(params[:page]).per(12)
-    @path = controller_path + '#' + action_name
-    def @path.is(*str)
-        str.map{|s| self.include?(s)}.include?(true)
-    end
-  	render template: "cd_products/list"
-  end
+     search_name = params[:search_name]
+   if search_name == ""
+     return redirect_to cms_path
+   end
+     @commodities = commodity_name_search_function(search_name)
+   @match_records = CdProduct.where(commodity_id:@commodities.ids).where(sale_status_id: 2).count
+   @commodities_cd_products = CdProduct.where(commodity_id:@commodities.ids).where(sale_status_id: 2).page(params[:page]).per(12)
+   @path = controller_path + '#' + action_name
+   def @path.is(*str)
+       str.map{|s| self.include?(s)}.include?(true)
+   end
+     render template: "cd_products/list"
+ end
 
-  def company_search
-    search_name = params[:search_name]
-    if search_name == ""
-      return redirect_to cms_path
-    end
-    @match_records = company_match_record_function(search_name)
-    @cd_products = company_name_search_function(search_name)
-    @path = controller_path + '#' + action_name
-    def @path.is(*str)
-        str.map{|s| self.include?(s)}.include?(true)
-    end
-    render template: "cd_products/list"
-  end
+ def company_search
+   search_name = params[:search_name]
+   if search_name == ""
+     return redirect_to cms_path
+   end
+   @companies = company_name_search_function(search_name)
+   @companies_commodities = Commodity.where(company_id:@companies.ids)
+   @match_records = CdProduct.where(commodity_id:@companies_commodities.ids).where(sale_status_id: 2).count
+   @companies_cd_products = CdProduct.where(commodity_id:@companies_commodities.ids).where(sale_status_id: 2).page(params[:page]).per(12)
+   @path = controller_path + '#' + action_name
+   def @path.is(*str)
+       str.map{|s| self.include?(s)}.include?(true)
+   end
+   render template: "cd_products/list"
+ end
 
 end
