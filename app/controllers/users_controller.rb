ebb6e1b6024af0ea120b8eea4f9f5c233d203d2e
pesopeso1234 <ito.taishi.0322@gmail.com>
2018-11-29 22:33:18 +0000

    modified ログイン周り

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
-before_action :login_check, except: :complete
+before_action :authenticate_user!, except: [:complete,:guide]
 # @user.address_lines.build ←newのときは書いたほうがいい
   def guide
   end
 
   def show
-    @user = User.active
+    @user = User.find(current_user.id)
     @favorites = current_user.favorites
     @shopping_bags = current_user.shopping_bags
   end
 
   def edit
-    @user = User.active
+    @user = User.find(current_user.id)
   end
 
   def update
-      @user = User.active
+      @user = User.find(current_user.id)
       # @user.id = current_user.id
     @user.update(user_params)
     redirect_to @user
@@ -37,6 +37,3 @@ private
       params.require(:user).permit(:first_name, :last_name, :first_name_kana,:last_name_kana, :email, :telephone, address_lines_attributes:[:id, :name, :postcode, :prefecture_id,:city,:address])
   end
 
-  def login_check
-      redirect_to new_user_registration_path unless user_signed_in?
-  end
